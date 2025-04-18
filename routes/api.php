82ef8d58eac351d7e23ba46d1b2c1d1135daf48e
AntoineCor35 <antoine.cormier@my-digital-school.org>
2025-04-18 13:41:43 +0200

    refactor: mise à jour des routes pour la nouvelle structure de l'application

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Groups;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,50 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/groups/{group}', function (Groups $group) {
+    Log::info('API - Chargement du groupe', [
+        'group_id' => $group->id,
+        'group_name' => $group->name
+    ]);
+
+    $group->load([
+        'projects' => function ($query) {
+            $query->with(['tags', 'media']);
+        }
+    ]);
+
+    Log::info('API - Données chargées', [
+        'projects_count' => $group->projects->count(),
+        'projects' => $group->projects->map(function ($project) {
+            return [
+                'id' => $project->id,
+                'name' => $project->name,
+                'media_count' => $project->media->count()
+            ];
+        })
+    ]);
+
+    return response()->json([
+        'id' => $group->id,
+        'name' => $group->name,
+        'description' => $group->description,
+        'projects' => $group->projects->map(function ($project) {
+            return [
+                'id' => $project->id,
+                'name' => $project->name,
+                'description' => $project->description,
+                'tags' => $project->tags,
+                'media' => $project->media->map(function ($media) {
+                    return [
+                        'id' => $media->id,
+                        'url' => $media->getUrl(),
+                        'original_url' => $media->getUrl(),
+                        'thumb_url' => $media->getUrl('thumb'),
+                        'preview_url' => $media->getUrl('preview')
+                    ];
+                })
+            ];
+        })
+    ]);
+})->middleware('auth:sanctum');
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::get('/dashboard/groups/{group}', [DashboardController::class, 'showGroup'])->middleware(['auth'])->name('dashboard.group');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
